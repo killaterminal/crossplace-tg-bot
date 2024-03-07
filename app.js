b5d5f8abbc781cd7631efce6eeafa2823551c21b
killaterminal <157806879+killaterminal@users.noreply.github.com>
2024-03-07 17:13:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const token = '6256350860:AAG4zBfGIcP1mNEimo4hyTZ9Yoiz6ndm-Ok';
+const app = express();
+const port = 3020;
 
-const bot = new TelegramBot(token, { polling: true });
+mongoose.connect('mongodb+srv://admin:123zxc34@cluster0.hoxv5bc.mongodb.net/crossplace', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Підключено до MongoDB'))
+    .catch(err => console.error('Помилка підключення до MongoDB:', err));
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Пошёл нахуй) Я классный хуй. Как я могу тебя уложить под шконку, пидор?');
+const securitySchema = new mongoose.Schema({
+    name: String,
+    category: String,
+    price: Number,
+    description: String,
+    image: String
+});
+const Security = mongoose.model('security', securitySchema);
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/securityItems', async (req, res) => {
+    try {
+        const securityItems = await Security.find({});
+        res.json(securityItems);
+    } catch (error) {
+        console.error('Помилка при отриманні даних з бази даних:', error);
+        res.status(500).send('Помилка сервера');
+    }
 });
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Я получил твою блядскую СМСку: ' + msg.text);
+app.listen(port, () => {
+    console.log(`Сервер запущено на порті ${port}`);
 });
