d02f28776870f434197f36e2d44390963f0dd95b
BeautifulBabe-MTM <155925393+antonprilepin@users.noreply.github.com>
2024-03-11 13:11:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const token = '6256350860:AAG4zBfGIcP1mNEimo4hyTZ9Yoiz6ndm-Ok';
 
 const bot = new TelegramBot(token, { polling: true });
 
+mongoose.connect('mongodb+srv://admin:123zxc34@cluster0.hoxv5bc.mongodb.net/crossplace', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Підключено до MongoDB'))
+  .catch(err => console.error('Помилка підключення до MongoDB:', err));
+
+const clientSchema = new mongoose.Schema({
+  userId: Number,
+  username: String,
+  phoneNumber: String,
+  firstName: String,
+  lastName: String,
+  orders: [String]
+});
+
+// Модель пользователя
+const Clients = mongoose.model('clients', clientSchema);
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const opts = {
@@ -46,10 +62,23 @@ bot.on('contact', (msg) => {
   const firstName = msg.contact.first_name;
   const lastName = msg.contact.last_name;
 
-  console.log(`UserId: ${userId}\nUsername: ${username}\nName: ${firstName}\nLast name: ${lastName}\nPhone number: ${phoneNumber}`);
-  bot.sendMessage(chatId, 'Дякуємо, що приєдналися до нас.', {
-    reply_markup: {
-      remove_keyboard: true
-    }
+  const newUser = new User({
+    userId: userId,
+    username: username,
+    phoneNumber: phoneNumber,
+    firstName: firstName,
+    lastName: lastName,
+    orders: []
   });
+
+  newUser.save((err, user) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Новый пользователь сохранен в базе данных:', user);
+  });
+
+  console.log(`UserId: ${userId}\nUsername: ${username}\nName: ${firstName}\nLast name: ${lastName}\nPhone number: ${phoneNumber}`);
+  bot.sendMessage(chatId, 'Дякуємо, що приєдналися до нас.');
 });
