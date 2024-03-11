0a303ac63ec192989dfd03a9c5540db49f71cfb4
antonprilepin <155925393+antonprilepin@users.noreply.github.com>
2024-03-11 12:48:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
 
 const token = '6256350860:AAG4zBfGIcP1mNEimo4hyTZ9Yoiz6ndm-Ok';
 
@@ -6,10 +7,43 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Привет! Я бот. Как я могу помочь?');
+  const opts = {
+    reply_markup: {
+      inline_keyboard: [[
+        {
+          text: 'Зареєструватися',
+          callback_data: 'register'
+        }
+      ]]
+    }
+  };
+  bot.sendMessage(chatId, 'Привіт! Натисни кнопку "Зареєструватися", щоб почати процес реєстрації.', opts);
 });
 
-bot.on('message', (msg) => {
+bot.on('callback_query', (query) => {
+  if (query.data === 'register') {
+    const chatId = query.message.chat.id;
+    bot.sendMessage(chatId, 'Будь-ласка, відправ свій контакт, щоб завершити реєстрацію.', {
+      reply_markup: {
+        keyboard: [
+          [{
+            text: 'Відправити номер телефону',
+            request_contact: true
+          }]
+        ],
+        resize_keyboard: true
+      }
+    });
+  }
+});
+
+bot.on('contact', (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, msg.text);
+  const userId = msg.from.id;
+  const phoneNumber = msg.contact.phone_number;
+  const firstName = msg.contact.first_name;
+  const lastName = msg.contact.last_name;
+
+  console.log(`Username: ${userId}\nName: ${firstName}\nLast name: ${lastName}\nPhone number: ${phoneNumber}`);
+  bot.sendMessage(chatId, 'Дякуємо, що приєдналися до нас.');
 });
